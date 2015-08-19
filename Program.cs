9225fd09a0efc8a61bbd6080327cd5cc32b9f6d1
Nguyen Ngoc Tung <ngoctung_dhbk@yahoo.com>
2015-08-19 09:17:58 +0700

    Implement featureB

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace Hello_C_sharp
         {
             Console.WriteLine("Hello world {0}", 1);
             Console.WriteLine("Implement featureA");
+            Console.WriteLine("Implement featureB");
             Console.ReadKey();
         }
     }
