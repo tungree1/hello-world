f31c40c20f7384062f9cf9094ea1543542a0c474
Nguyen Ngoc Tung <ngoctung_dhbk@yahoo.com>
2015-08-19 08:26:15 +0700

    Initialize project folder

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello_C_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world {0}", 1);
+            Console.ReadKey();
+        }
+    }
+}
